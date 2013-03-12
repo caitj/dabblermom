639d468ceabd2787b3387597f30c63077373e7a6
Caitlin Jackson <caitlin.alyssa@thinkhard.net>
2013-03-12 00:18:38 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dabblermom::Application.config.secret_token = 'dbf00e624a26303663d5440f4d6ec2634f1c32612afec1ae52b8714d2cec5bdcec4404db3d527af87504bcc96d267e0213c83d284e9934a759e5c11e2cd9b5af'
